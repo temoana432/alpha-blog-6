923cd83a5d56624095218214066edc5a9a89797e
temoana432 <SteveClarke40@hotmail.com>
2022-02-25 15:33:47 -0700

    end of chap151

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
     def gravatar_for(user, options = {size: 80})
-    email_address = user.email.downcase
+        email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
         gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         # SMC TODO - look at the user of alt: here
-    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
 end
